3310deccbe1647f86f1e96f6849e406ca2309262
Fryderyk Boncler <f.relcnob@gmail.com>
2022-09-05 17:28:06 +0200

    find all 3 names

--- nameparts.js
+++ nameparts.js
@@ -1,2 +1,11 @@
 ("use strict");
-const name = `Peter Heronimous Lind`;
+const fullName = `Peter Heronimous Lind`;
+
+const firstNameEnd = fullName.indexOf(` `);
+const fistName = fullName.substring(0, firstNameEnd);
+const middleNameEnd = fullName.indexOf(` `, firstNameEnd + 1);
+const middleName = fullName.substring(firstNameEnd + 1, middleNameEnd);
+const lastName = fullName.substring(middleNameEnd + 1);
+console.log(fistName);
+console.log(middleName);
+console.log(lastName);
